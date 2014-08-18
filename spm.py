6e57fecfe119891379b81d47383754e8ed980991
Yannick Schwartz <yannick.schwartz@cea.fr>
2014-08-18 11:38:54 +0200

    update spm

--- spm.py
+++ spm.py
@@ -9,7 +9,6 @@ import pandas as pd
 from scipy.io import loadmat
 
 
-
 def load_matfile(mat_file):
     if isinstance(mat_file, (str, unicode)):
         if mat_file.endswith('.gz'):
@@ -39,14 +38,21 @@ def load_conditions_onsets(mat_file, session_names=None, condition_names=None):
                 durations = [durations]
             n_events = len(onsets)
             amplitudes = [1] * n_events
-            session_name = session_names.get(session_id + 1, 'session%03i' % (session_id + 1))
-            condition_name = condition_names.get((session_id + 1, condition_id + 1), default_condition_name)
+            session_name = session_names.get(
+                session_id + 1,
+                'session%03i' % (session_id + 1))
+            condition_name = condition_names.get(
+                (session_id + 1, condition_id + 1),
+                default_condition_name)
             for i in range(n_events):
                 conditions.setdefault('session_id', []).append(session_id + 1)
                 conditions.setdefault('session_name', []).append(session_name)
-                conditions.setdefault('condition_id', []).append(condition_id + 1)
-                conditions.setdefault('condition_name', []).append(condition_name)
-                conditions.setdefault('condition_default_name', []).append(default_condition_name)
+                conditions.setdefault('condition_id', []).append(
+                    condition_id + 1)
+                conditions.setdefault('condition_name', []).append(
+                    condition_name)
+                conditions.setdefault('condition_default_name', []).append(
+                    default_condition_name)
                 conditions.setdefault('onset', []).append(onsets[i])
                 conditions.setdefault('duration', []).append(durations[i])
                 conditions.setdefault('amplitude', []).append(amplitudes[i])
@@ -54,16 +60,59 @@ def load_conditions_onsets(mat_file, session_names=None, condition_names=None):
     return pd.DataFrame(conditions)
 
 
-def save_conditions_onsets(conditions_onsets, onsets_dir=tempfile.gettempdir()):
+def save_conditions_onsets(conditions_onsets,
+                           onsets_dir=tempfile.gettempdir()):
     condition_files = []
 
-    for cid, cframe in conditions_onsets.groupby(['session_name', 'condition_id']):
+    for cid, cframe in conditions_onsets.groupby(['session_name',
+                                                  'condition_id']):
         session_id, condition_id = cid
         onset_dir = os.path.join(onsets_dir, session_id)
         if not os.path.exists(onset_dir):
             os.makedirs(onset_dir)
         fname = os.path.join(onset_dir, 'cond%03i.txt' % condition_id)
-        cframe[['onset', 'duration', 'amplitude']].to_csv(fname, sep=' ', header=False, index=False)
+        cframe[['onset', 'duration', 'amplitude']].to_csv(
+            fname, sep=' ', header=False, index=False)
         condition_files.append(fname)
 
     return condition_files
+
+
+def get_task_contrasts(mat_file, has_derivative=True):
+    matfile = load_matfile(mat_file)['SPM']
+    mat_dir = os.path.abspath(os.path.split(mat_file)[0])
+
+    stats = {}
+
+    if hasattr(matfile.Sess, '__iter__'):
+        sessions = matfile.Sess
+    else:
+        sessions = [matfile.Sess]
+
+    conditions = []
+    for session_id, session in enumerate(sessions):
+        for i, u in enumerate(session.U):
+            condition_id = 'cond%03i_%s' % (i + 1, str(u.name))
+            conditions.append(condition_id)
+            if has_derivative:
+                conditions.append('%s_derivative' % condition_id)
+
+    for c in matfile.xCon:
+        image_id = str(c.name)
+
+        try:
+            c_map = os.path.join(mat_dir, str(c.Vcon.fname))
+            t_map = os.path.join(mat_dir, str(c.Vspm.fname))
+        except:
+            c_map = ''
+            t_map = ''
+
+        stats.setdefault('image_id', []).append(image_id)
+        stats.setdefault('c_maps', []).append(c_map)
+        stats.setdefault('t_maps', []).append(t_map)
+
+        conditions_values = c.c.tolist()[:len(conditions)]
+        for condition_id, v in zip(conditions, conditions_values):
+            stats.setdefault(condition_id, []).append(v)
+
+    return pd.DataFrame(stats)
